66cfa6216004b4d75974b6c563e98b76075b8ce2
rivuff <rivunaskar0@gmail.com>
2022-12-18 13:55:11 +0530

    added Airports model

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,5 @@
 const { City } = require('../models/index');
+const { Op } = require('sequelize');
 
 class CityRepository{
 
@@ -58,8 +59,19 @@ class CityRepository{
         }
     }
 
-    async getAllCities(){
+    async getAllCities(filter){
         try {
+
+            if(filter.name){
+                const cities = await City.findAll({
+                    where:{
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
