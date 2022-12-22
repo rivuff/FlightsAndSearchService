ef7b73fd0fa7c345b408cff46f8f4009b21c05fc
rivuff <rivunaskar0@gmail.com>
2022-12-22 13:09:44 +0530

    added filters in flights service

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -2,6 +2,21 @@ const {Flights} = require('../models/index');
 
 class FlightRepository{
 
+    #createFilter(data){
+        let filter = {}
+        if(data.arrivalAirportId){
+            filter.arrivalArrivalAirportId = data.arrivalArrivalAirportId;
+        }
+        if(data.departureAirportId){
+            filter.departureAirportId = data.departureAirportId;
+        }
+        if(data.minPrice){
+            Object.assign(filter,{price: {[Op.gte]: data.minPrice}});
+        }
+
+        return filter
+    }
+
     async createFlight(data){
         try {
             const flight =await Flights.create(data);
@@ -11,6 +26,28 @@ class FlightRepository{
             throw(error);
         }
         
+    }
+
+    async getFlight(flightId){
+        try {
+            const flight = await Flights.findByPk(flightId);
+            return flight;
+        } catch (error) {
+            console.log("something went wrong in repository layer");
+            throw(error);
+        }
+    }
+
+    async getAllFlights(filter){
+        try {
+            const fliterObject = this.createFilter(filter);
+            const flights = await Flights.findall({
+                where: fliterObject
+            });
+            return flights;
+        } catch (error) {
+            
+        }
     }
 }
 
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,4 +1,5 @@
 const { FlightRepository, AirplaneRepository } = require('../repository/index');
+const { compareTime } = require('../utils/helper')
 
 class FlightService{
     constructor(){
@@ -8,6 +9,10 @@ class FlightService{
 
     async createFLight(data){
         try {
+
+            if(!compareTime(data.arrivalTime, data.departureTime)) {
+                throw {error: 'Arrival time cannot be earlier than departure time'};
+            }
             const airplane = await this.AirplaneRepository.getAirplane(data.airplaneId);
             const flight = await this.FlightRepositry.createFlight({...data, totalSeats:airplane.capacity})
             return flight;
